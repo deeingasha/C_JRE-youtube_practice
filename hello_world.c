cd435006c5f076060a5046e505602b0da994b0a8
Dorcas <dorcasingasha@gmail.com>
2022-10-20 10:33:20 +0000

    Hello World program

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+  * main - Entry point
+  * Simple program to output Hello world
+  *
+  * Return: Always 0 --> Success
+  */
+
+int main(void)
+{
+	printf("Hello World\n");
+	return (0);
+}
